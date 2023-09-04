b7cac74b49427dfd46c18c3465fe25ba3489d914
Egor F <dr.egor-feofanov@yandex.ru>
2023-09-05 01:26:08 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+interface Props {
+  message: string;
+}
+
+const props = defineProps<Props>();
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ props.message }}</h1>
+
+  <div class="card">
+    <p>
+      builded and deployed
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
